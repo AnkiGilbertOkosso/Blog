f474483ac0e4d60cfe6677b04c2c4509963796bb
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-11-28 12:47:41 +0100

    reading all of my blog post at once

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,36 @@
 @section('title', '| View Post')
     
 @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1> {{ $post->title }} </h1>
+            <p class="lead"> {{ $post->body }} </p>
+        </div>
+        <div class="col-md-4">
+            <div class="card --bs-secondary-bg-rgb">
+                <div class="card-body">
+                    <dl class="dl-horizontal">
+                        <dt>Created At:</dt>
+                        <dd> {{ date('M j, Y h:i A', strtotime($post->created_at)) }} </dd>
+                    </dl>
 
-    <h1> {{ $post->title }} </h1>
-    <p class="lead"> {{ $post->body }} </p>
+                    <dl class="dl-horizontal">
+                        <dt>Last Updated:</dt>
+                        <dd> {{ date('M j, Y h:i A', strtotime($post->updated_at)) }}  </dd>
+                    </dl>
 
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                        </div>
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
