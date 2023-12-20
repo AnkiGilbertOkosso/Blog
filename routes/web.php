e3ca884243c8da64cf0f40ad860e0944a97e76d4
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-12-20 20:46:01 +0100

    updated

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/contact', [PageController::class, 'contact']);
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('posts', PostController::class)->parameters([ 'posts' => 'slug']);
+    Route::resource('posts', PostController::class)->parameters(['posts' => 'slug']);
     Route::post('/posts/{post}/comments', [CommentController::class, 'store'])
         ->name('comments.store');
     Route::resource('categories', CategoryController::class);
@@ -38,10 +38,6 @@ Route::middleware(['auth'])->group(function () {
     });
 });
 
-
-
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -51,7 +47,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 
 // Route::middleware(['auth', 'verified'])->group(function () {
 //     Route::get('/dashboard', function () {
